3093ec7023fa98cd427e08e25ccc6debee26ab1f
tszwingsuen <43230644+suentsz96wing@users.noreply.github.com>
2022-10-24 13:54:05 -0400

    no message

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # schedule_us_visa_in_canada
+
+###### Preparation
+
+1. Install package in Terminal, input
+   `yarn install`
+2. Create a new file called `.env`
+3. In `.env`, input your account details and information
+
+   ```
+   USERNAME={Your username}
+   PASSWORD={Your password}
+   LOCATION_ID={the number of location id}
+   MAX_MONTH={the number of month}
+   ```
+
+   LOCATION_ID:
+
+   - Calgary: 89
+   - Halifax: 90
+   - Montreal: 91
+   - Ottawa: 92
+   - Quebec City: 93
+   - Toronto: 94
+   - Vancouver: 95
+
+   MAX_MONTH:
+   Input the number of maximum following month
+
+   Example:
+
+   ```
+   USERNAME=myemail@gmail.com
+   PASSWORD=P@ssw0rd
+   LOCATION_ID=94
+   MAX_MONTH=6
+   ```
+
+###### Start Program in Terminal
+
+`node index.js`
---
+++ index.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://ais.usvisa-info.com/en-ca/niv/users/sign_in");
+  await page.waitForTimeout(2000);
+
+  await page.type("#user_email", process.env.USERNAME); //email
+  await page.type("#user_password", process.env.PASSWORD); //password
+  await page.click("#policy_confirmed"); // click accept policy
+  await page.click("[name='commit']"); // Submit button
+  await page.waitForTimeout(2000);
+  await page.click("a[class='button primary small']"); //Continue button
+  await page.waitForTimeout(2000);
+  await page.click("li[class='accordion-item']"); // Schedule accordion button
+  await page.waitForTimeout(1500);
+  await page.click("a[class='button small primary small-only-expanded']"); // Schedule button
+  await page.waitForTimeout(2000);
+  async function scheduleUSVisa() {
+    console.log(new Date());
+    await page.select(
+      "#appointments_consulate_appointment_facility_id",
+      process.env.LOCATION_ID
+    );
+    await page.waitForTimeout(1000);
+
+    const isNotAvailableDisplay = await page.$eval(
+      "#consulate_date_time_not_available",
+      (el) => {
+        return getComputedStyle(el).getPropertyValue("display");
+      }
+    );
+    await page.waitForTimeout(1000);
+
+    if (isNotAvailableDisplay !== "block") {
+      await page.type("#appointments_consulate_appointment_date", "2023-01-01");
+
+      await page.waitForTimeout(1000);
+
+      let i = 0;
+      while (i <= +process.ENV.MAX_MONTH) {
+        const dayElement = await page.$("td[class=' undefined']");
+        if (dayElement !== null) {
+          dayElement.click();
+          await page.waitForTimeout(1000);
+          const timeOptions = await page.evaluate(() =>
+            Array.from(
+              document.querySelectorAll(
+                "#appointments_consulate_appointment_time option"
+              )
+            ).map((element) => element.value)
+          );
+          await page.select(
+            "#appointments_consulate_appointment_time",
+            timeOptions[1]
+          );
+          await page.click("[name='commit']"); // Submit button
+          break;
+        }
+        await page.waitForTimeout(1000);
+        const nextButton = await page.$(
+          "a[class='ui-datepicker-next ui-corner-all']"
+        );
+        if (nextButton !== null) {
+          nextButton.click();
+        }
+        console.log("nextButton", nextButton);
+        await page.waitForTimeout(1000);
+        const weekEnd = await page.$("th[class='ui-datepicker-week-end']");
+        if (weekEnd !== null) {
+          await weekEnd.hover();
+        }
+        i++;
+        await page.waitForTimeout(1000);
+      }
+      await scheduleUSVisa();
+    } else {
+      await page.reload();
+      await page.waitForTimeout(30000);
+      await scheduleUSVisa();
+    }
+  }
+  scheduleUSVisa();
+  //   await browser.close();
+})();
